cb44b316419405d0fbbd431ecf4eabff2dbfb13b
“Mardie328” <tolayinka328@gmail.com>
2022-06-22 23:04:24 +0100

    Modify my javascript file

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-alert("My name: Taofeek Mardiyyah");
-alert("My height: 63.5 inches");
-alert("My country: Nigeria");
+//A code that prints my name, height and country.
+
+var myName = "Taofeek Mardiyyah";
+var myHeight = 63.5;
+var myCountry = "Nigeria";
+
+alert("My name is " + myName + ". I am " + myHeight + " inches tall." + " The name of my country is " + myCountry + "."); 
+
